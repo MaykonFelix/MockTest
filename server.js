927e598aa8defedeff62619a9b423ae0bb8cdb32
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-03-13 19:05:20 -0300

    Update

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
-// JSON Server module
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-// Make sure to use the default middleware
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
+server.use(middlewares)
 // Add this before server.use(router)
-server.use(
-    // Add custom route here if needed
-    jsonServer.rewriter({
-        "/api/*": "/$1",
-    })
-);
-server.use(router);
-// Listen to port
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
 server.listen(3000, () => {
-    console.log("JSON Server is running");
-});
+    console.log('JSON Server is running')
+})
 
 // Export the Server API
-module.exports = server;
+module.exports = server
